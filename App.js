998a5d4cfd9f0b2dca95e62cc19d2ee7e7e98ed2
Nivetha Siva <nive0204@gmail.com>
2020-07-02 12:31:58 -0400

    - Initial checkin of starter React Native project

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function App() {
+  console.log('hello');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nivetha Siva</Text>
+      <Button title='My first button' type='outline'/>
       <StatusBar style="auto" />
     </View>
   );
